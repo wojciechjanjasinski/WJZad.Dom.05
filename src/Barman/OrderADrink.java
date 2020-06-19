6d2b248125b400226f062582e47d7173b58ecb33
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-19 21:10:56 +0200

    Getting more clean code in Barman package.

--- src/Barman/OrderADrink.java
+++ src/Barman/OrderADrink.java
@@ -5,16 +5,16 @@ public class OrderADrink {
         Barman barman = new Barman();
 
         Person personOne = new Person("Jan", "Kazimierski", 17, new Drink("Mroczna noc", true, 20.99));
-        orderTwo(barman, personOne);
+        order(barman, personOne);
 
         Person personTwo = new Person("Anna", "Lemańska", 15, new Drink("Radość o poranku", false, 15.99));
-        orderTwo(barman, personTwo);
+        order(barman, personTwo);
 
         Person personThree = new Person("Albert", "Kumański", 20, new Drink("Jasna pani", true, 22.99));
-        orderTwo(barman, personThree);
+        order(barman, personThree);
     }
 
-    private static void orderTwo(Barman barman, Person personOne) {
+    private static void order(Barman barman, Person personOne) {
         if (barman.order(personOne)) {
             System.out.println(personOne.getFirstName() + " Twój drink " + personOne.getDrink().getName() + " jest gotowy do odbioru. Kwota do zapłaty to " + personOne.getDrink().getPrice() + " PLN ");
         } else {
